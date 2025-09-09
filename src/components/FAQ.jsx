9101013d7cfa149674d65abfdaaae6b649995ce6
Jane Doe <alsousha@mail.ru>
2025-09-09 22:22:25 +0300

    faq with css

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -16,19 +16,33 @@ function FAQ() {
       answer: 'Можно создать функциональный или классовый компонент с помощью функций или классов.',
     },
   ];
-  const [openIndex, setOpenIndex] = useState(null);
+  //this for one open
+  //   const [openIndex, setOpenIndex] = useState(null);
+  const [openIndexes, setOpenIndexs] = useState([]);
+
+  const toggleIndex = (index) => {
+    if (openIndexes.includes(index)) {
+      //если уже открыт - закрывает. оставляем только те эл, которые не равны текущ инд
+      setOpenIndexs(openIndexes.filter((i) => i !== index));
+    } else {
+      //add to openIndexes
+      setOpenIndexs([...openIndexes, index]);
+    }
+  };
   return (
     <div className='faq'>
       {faqData.map((item, ind) => {
         console.log(item);
-        const isOpen = openIndex === ind;
+        //   const isOpen = openIndex === ind;
+        const isOpen = openIndexes.includes(ind);
         return (
           <div className={`faq-item ${isOpen ? 'active' : ''}`}>
             <div
-              className='faq-quetion  d-flex jcsb'
+              className='faq-quetion  d-flex jcsb aic'
               //если текущий открытый элемент (openIndex) уже равен индексу элемента, по которому кликнули - юзер хочет закрыть этот эл-т
               //елси текущий открытый элемент не равен openIndex значит хочет открыть
-              onClick={() => setOpenIndex(isOpen ? null : ind)}>
+              //   onClick={() => setOpenIndex(isOpen ? null : ind)}>
+              onClick={() => toggleIndex(ind)}>
               <h3 className='faq-title'>{item.question}</h3>
               <img src={arrow} alt='' className='faq-arrow' />
             </div>
